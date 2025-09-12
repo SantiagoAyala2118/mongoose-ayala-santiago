4effbce13402a67e0a710c4b616b58ea72e4a470
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-12 20:34:42 -0300

    Ruta del archivo index importada

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { routes } from "./src/routes/index.js";
+import { startDB } from "./src/config/database.js";
+startDB();
 
 const app = express();
-const PORT = 3000;
+const PORT = 4100;
 
+//--------------MIDDLEWARES
 app.use(express.json());
+
+//--------------RUTAS
+app.use("/api", routes);
 
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
