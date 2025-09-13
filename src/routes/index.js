951881b70f95cf6e9b14edc04874cde56198099d
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-13 09:45:07 -0300

    Ruta de profile agregada

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 //-------------------RUTAS
 import { userRoutes } from "./user.routes.js";
+import { profileRoutes } from "./profile.routes.js";
 
 export const routes = Router();
 
 //RUTAS DE USER
 routes.use(userRoutes);
+
+//RUTAS DE PROFILE
+routes.use(profileRoutes);
