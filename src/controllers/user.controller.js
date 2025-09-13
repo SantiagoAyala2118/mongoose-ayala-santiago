3b6f3eb0f2cf64e4399ed9a59fc957ed8d4b5018
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-12 22:55:35 -0300

    Controladores restantes completados y funcionando (puestos a modificación)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -41,3 +41,67 @@ export const getAllUsers = async (req, res) => {
     });
   }
 };
+
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json({
+      ok: true,
+      message: "Here is the user",
+      User: user,
+    });
+  } catch (err) {
+    console.error("Server error", err);
+    return res.status(500).json({
+      ok: false,
+      message: "Server error",
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password, favorite_game } = req.body;
+    const updateUser = await UserModel.findByIdAndUpdate(
+      id,
+      username,
+      email,
+      password,
+      favorite_game
+    );
+
+    return res.status(200).json({
+      opk: true,
+      message: "User updated",
+      User: updateUser,
+    });
+  } catch (err) {
+    console.error("Server error", err);
+    return res.status(500).json({
+      ok: false,
+      message: "Server error",
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteUser = await UserModel.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      ok: true,
+      message: "User deleted",
+      user_deleted: deleteUser,
+    });
+  } catch (err) {
+    console.error("Server error", err);
+    return res.status(500).json({
+      ok: false,
+      message: "User deleted",
+    });
+  }
+};
