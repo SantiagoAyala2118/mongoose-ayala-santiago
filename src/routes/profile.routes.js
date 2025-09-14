a692e509ec8d1cfd52a4976028ac656b1c68129e
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-14 17:50:18 -0300

    Middlewares de validaciones agregados

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+
+//-------------------------CONTROLADORES
 import {
   createProfile,
   deleteProfile,
@@ -7,19 +9,48 @@ import {
   updateProfile,
 } from "../controllers/profile.controller.js";
 
+//---------------------------MIDDLEWARES
+import {
+  createProfileValidations,
+  getProfileValidations,
+  updateProfileValidations,
+  deleteProfileValidations,
+} from "../middlewares/validations/profile.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
+
 export const profileRoutes = Router();
 
 //CREAR PERFIL
-profileRoutes.post("/profile", createProfile);
+profileRoutes.post(
+  "/profile",
+  createProfileValidations,
+  applyValidations,
+  createProfile
+);
 
 //TRAER TODOS LOS PERFILES
 profileRoutes.get("/profile", getAllProfiles);
 
 //TRAER UN SOLO PERFIL
-profileRoutes.get("/profile/:id", getProfile);
+profileRoutes.get(
+  "/profile/:id",
+  getProfileValidations,
+  applyValidations,
+  getProfile
+);
 
 //ACTUALIZAR UN PERFIL
-profileRoutes.put("/profile/:id", updateProfile);
+profileRoutes.put(
+  "/profile/:id",
+  updateProfileValidations,
+  applyValidations,
+  updateProfile
+);
 
 //ELIMINAR UN PERFIL
-profileRoutes.delete("/profile/:id", deleteProfile);
+profileRoutes.delete(
+  "/profile/:id",
+  deleteProfileValidations,
+  applyValidations,
+  deleteProfile
+);
