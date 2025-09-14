3a28a5c1119e47661a7bb313e2dc0d40d5750584
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-14 17:49:58 -0300

    Middlewares de validaciones agregados

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+//------------------CONTROLADORES
 import {
   createUser,
   deleteUser,
@@ -7,19 +8,38 @@ import {
   updateUser,
 } from "../controllers/user.controller.js";
 
+//-------------------MIDDLEWARES
+import {
+  createUserValidations,
+  getUserValidations,
+  updateUserValidations,
+  deleteUserValidations,
+} from "../middlewares/validations/user.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
+
 export const userRoutes = Router();
 
 //CREAR USUARIO
-userRoutes.post("/users", createUser);
+userRoutes.post("/users", createUserValidations, applyValidations, createUser);
 
 //TRAER USUARIOS
 userRoutes.get("/users", getAllUsers);
 
 //TRAER UN USUARIO
-userRoutes.get("/users/:id", getUser);
+userRoutes.get("/users/:id", getUserValidations, applyValidations, getUser);
 
 //ACTUALIZAR UN USUARIO
-userRoutes.put("/users/:id", updateUser);
+userRoutes.put(
+  "/users/:id",
+  updateUserValidations,
+  applyValidations,
+  updateUser
+);
 
 //BORRAR UN USUARIO
-userRoutes.delete("/users/:id", deleteUser);
+userRoutes.delete(
+  "/users/:id",
+  deleteUserValidations,
+  applyValidations,
+  deleteUser
+);
