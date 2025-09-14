f6c80dbe6c13b81827f83aaa103caade715b5a54
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-14 19:22:14 -0300

    Validaciones agregadas

--- src/routes/user_group.routes.js
+++ src/routes/user_group.routes.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
+
+//-------------------CONTROLADORES
 import { createUserGroup } from "../controllers/user_group.controller.js";
+
+//-------------------MIDDLEWARES
+import { createUserGroupValidations } from "../middlewares/validations/user_group.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 export const userGroupRoutes = Router();
 
 //ENDPOINT PARA LA RELACIÓN DE MUCHOS A MUCHOS
-userGroupRoutes.post("/user-groups/:id", createUserGroup);
+userGroupRoutes.post(
+  "/user-groups/:id",
+  createUserGroupValidations,
+  applyValidations,
+  createUserGroup
+);
