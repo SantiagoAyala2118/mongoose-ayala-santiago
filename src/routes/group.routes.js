07761ae44002ad3ae5c7b49b0bd8e1d6823da2b5
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-14 18:40:13 -0300

    Middlewares de validaciones añadidos

--- src/routes/group.routes.js
+++ src/routes/group.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+
+//-----------------------CONTROLADORES
 import {
   createGroup,
   deleteGroup,
@@ -7,19 +9,43 @@ import {
   updateGroup,
 } from "../controllers/group.controller.js";
 
+//------------------------MIDDLEWARES
+import {
+  createGroupValidations,
+  getGroupValidations,
+  updateGroupValidations,
+  deleteGroupValidations,
+} from "../middlewares/validations/group.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
+
 export const groupRoutes = Router();
 
 //CREAR UN GRUPO
-groupRoutes.post("/groups", createGroup);
+groupRoutes.post(
+  "/groups",
+  createGroupValidations,
+  applyValidations,
+  createGroup
+);
 
 //TRAER TODOS LOS GRUPOS
 groupRoutes.get("/groups", getAllGroups);
 
 //TRAER UN GRUPO
-groupRoutes.get("/groups/:id", getGroup);
+groupRoutes.get("/groups/:id", getGroupValidations, applyValidations, getGroup);
 
 //ACTUALIZAR UN GRUPO
-groupRoutes.put("/groups/:id", updateGroup);
+groupRoutes.put(
+  "/groups/:id",
+  updateGroupValidations,
+  applyValidations,
+  updateGroup
+);
 
 //ELIMINAR UN GRUPO
-groupRoutes.delete("/groups/:id", deleteGroup);
+groupRoutes.delete(
+  "/groups/:id",
+  deleteGroupValidations,
+  applyValidations,
+  deleteGroup
+);
